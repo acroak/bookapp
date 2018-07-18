ec98f23773a6e3056a05d3fc69646480dc063e0d
acroak <acroak@live.com>
2018-07-18 18:40:43 -0400

    submit before break, working on backend curl

--- controllers/books.js
+++ controllers/books.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/books.js')
-
+//show all books... INDEX
 router.get('/', (req, res)=> {
+  //res.send('index')
   Book.find({}, (err, foundBook)=>{
     console.log('found a book!');
+    res.json(foundBook)
   })
 });
 
 //add a book!
 router.post('/',(req,res)=>{
   Book.create(req.body,(err, createdBook)=>{
+    res.json(createdBook);
     console.log('we created a book!');
   })
 });
 //curl -X POST -H "Content-Type: application/json" -d '{"title":"", "author":"", "genre":""}' http://localhost:3000/books
-//curl -X POST -H "Content-Type: application/json" -d '{"title":"Harry Potter and the Sorcers Stone", "author":"J.K.Rowling", "genre":"Young Adult"}' http://localhost:3000/books
+//curl -X POST -H "Content-Type: application/json" -d '{"title":"Harry Potter and the Sorcerers Stone", "author":"J.K.Rowling", "genre":"Young Adult"}' http://localhost:3000/books
 
 module.exports = router;
--- models/books.js
+++ models/books.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const bookSchema = Schema({
+const bookSchema = new mongoose.Schema({
     title: String,
     author: String,
     genre: String
