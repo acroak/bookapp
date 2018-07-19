b75c16fcf0f152de1770646b5b9cae2e0c34cb73
acroak <acroak@live.com>
2018-07-18 20:02:48 -0400

    merger issue

--- models/books.js
+++ models/books.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const bookSchema = new mongoose.Schema({
+const bookSchema = new Schema({
     title: String,
     author: String,
     genre: String
