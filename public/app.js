1ef25154a1ecd5deb26805fd5122af34396d8588
acroak <acroak@live.com>
2018-07-18 20:34:47 -0400

    working on delete route

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
 const app = angular.module('BookApp', []);
 
 app.controller('MainController', ['$http', function($http) {
-   this.foo = 'bar'
-   const controller = this;
+  // this.foo = 'bar'
+  //const controller = this;
+   this.books = [];
+   this.createForm = {};
+   this.book = '';
    this.createBook = () => {
      //console.log('making a book');
      $http({
@@ -19,15 +22,34 @@ app.controller('MainController', ['$http', function($http) {
        console.log(error);
      })
    }//end createBook function
+
    this.getBooks = () => {
-$http({
-  method:'GET',
-  url:'/books',
-}).then(response => {
-  this.books = response.data;
-}, error => {
-  console.log(error);
-})
-}
+    $http({
+      method:'GET',
+      url:'/books',
+    }).then(response => {
+      this.books = response.data;
+    }, error => {
+      console.log(error);
+    })
+    }
+
+
+    this.deleteBook = (id) =>{
+      console.log('Deleting...', id);
+      $http({
+        method: 'DELETE',
+        url: '/books/' + id
+      }).then( response => {
+        
+        this.getBooks();
+      // const removeByIndex = this.books.findIndex(book => book._id === id)
+      // this.books.splice(removeByIndex, 1)
+        //console.log(response.data);
+        console.log('removed book!');
+      }, error =>{
+        console.log(error);
+      })
+    }
 this.getBooks()
 }]); //app controller ends here.
